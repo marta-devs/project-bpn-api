a5af838b4ea7a5a0bb6a4803c74f451953dab20d
Walany Costa <walanybnegro@gmail.com>
2025-04-27 15:46:53 +0100

    refactor: rename methods and class in file DAO

--- src/repositories/DAO.ts
+++ src/repositories/DAO.ts
@@ -1,7 +1,7 @@
-export interface GenericDAO<T = any> {
-	findAll: () => Promise<T[]>;
-	findById: (id: string) => Promise<T | null>;
-	create: (data: T) => Promise<T>;
-	update: (id: string, data: T) => Promise<T>;
-	delete: (id: string) => Promise<T | null>;
+export interface DAOGenerico<T = any> {
+	buscarTodos: () => Promise<T[]>;
+	buscarPorId: (id: string) => Promise<T | null>;
+	criar: (dados: T) => Promise<T>;
+	atualizar: (id: string, dados: T) => Promise<T>;
+	excluir: (id: string) => Promise<T | null>;
 }
